fc4232ca706c126e63da4913063d5e695dc6e625
Ayumi <lulu_nishiyama@hotmail.com>
2023-12-22 18:37:01 -0300

    "Atualização número máximo 100 para 50"

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoTela(tag, texto){
 
 function mensagemInicial(){
     exibirTextoTela("h1", "Número Secreto");
-    exibirTextoTela("p", "Escolha um número entre 1 e 100");
+    exibirTextoTela("p", "Escolha um número entre 1 e 50");
 }
 
 mensagemInicial();
@@ -38,7 +38,7 @@ function verificarChute() {
 }
 
 function gerarNumero() {
-    let numeroEscolhido = parseInt(Math.random()*100 + 1);
+    let numeroEscolhido = parseInt(Math.random()*50 + 1);
 
     if(listaNumeros==3){
         listaNumeros=[];
